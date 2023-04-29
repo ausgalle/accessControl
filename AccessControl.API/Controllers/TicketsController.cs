6158b855d7f8126dc6d4605f40c9524a0193d55e
ALEJANDRO USME GALLEGO <alejandrousme235808@correo.itm.edu.co>
2023-04-28 21:33:35 -0500

    Create a seed and controller of tcket

---
+++ AccessControl.API/Controllers/TicketsController.cs
@@ -0,0 +1,30 @@
+﻿using AccessControl.API.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace AccessControl.API.Controllers
+{
+    [ApiController]
+    [Route("/api/tickets")]
+    public class TicketsController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public TicketsController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult> GetAsync(int id)
+        {
+            var ticket = await _context.mvTickets.FirstOrDefaultAsync(x => x.Id == id);
+            if (ticket == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(ticket);
+        }
+    }
+}
